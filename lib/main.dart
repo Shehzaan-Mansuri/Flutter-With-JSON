eb60573766b5a3206f5fcccb79b745cd6f483648
Shehzaan-Mansuri <shehzaanmansuri1@gmail.com>
2021-07-12 22:24:04 +0530

    Flutter JSON

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: HomePage(),
+        theme: ThemeData(primarySwatch: Colors.teal),
+      ),
+    );
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  
+  List data;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Load JSON App"),
+      ),
+      body: Container(
+        child: Center(
+          child: FutureBuilder(
+            future: DefaultAssetBundle
+                .of(context)
+                .loadString('load_json/person.json'),
+            builder: (context,  snapshot) {
+
+              var mydata = json.decode(toString());
+
+              return ListView.builder(
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Text("Name: " + mydata[index]['name']),
+                        Text("Age: " + mydata[index]['age']),
+                        Text("Hair Color: " + mydata[index]['hair color']),
+                        Text("Gender: " + mydata[index]['gender']),
+                      ],
+                    ),
+                  );
+                },
+                itemCount: mydata == null ? 0 : mydata.length,
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
